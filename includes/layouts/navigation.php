63451f688bb3cb5cb197daad755c616cf830302f
miladpeagh <miladpeagh1999@gmail.com>
2021-08-28 19:42:05 +0430

    response navbar

--- includes/layouts/navigation.php
+++ includes/layouts/navigation.php
@@ -1,9 +1,9 @@
 <nav id="top_menu">
     <ul>
-        <li><a href="index.php" id="home"><span>HOME</span></a></li>
-        <li><a href="about.php" id="about"><span>ABOUT</span></a></li>
-        <li><a href="signup.php" id="membership"><span>MEMBERSHIP</span></a></li>
-        <li><a href="Trainers.php" id="trainer"><span>TRAINERS</span></a></li>
-        <li><a href="contact.php" id="contact"><span>CONTACT</span></a></li>
+        <li style="padding-right: 2%;"><a href="index.php" id="home"><span>HOME</span></a></li>
+        <li style="padding-right: 2%;"><a href="about.php" id="about"><span>ABOUT</span></a></li>
+        <li style="padding-right: 2%;"><a href="signup.php" id="membership"><span>MEMBERSHIP</span></a></li>
+        <li style="padding-right: 2%;"><a href="Trainers.php" id="trainer"><span>TRAINERS</span></a></li>
+        <li style="padding-right: 2%;"><a href="contact.php" id="contact"><span>CONTACT</span></a></li>
     </ul>
 </nav>
